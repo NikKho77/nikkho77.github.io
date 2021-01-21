0f41d6876292e271b015869bf2bed0f46da0ab28
NikKho77 <68790627+NikKho77@users.noreply.github.com>
2021-01-21 15:52:26 +0200

    Add files via upload
    sde

---
+++ carusel.js
@@ -0,0 +1,28 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImg = document.querySelectorAll('.carousel-slide img');
+const nextBtn = document.querySelector('#next');
+
+let counter = 1;
+const size = carouselImg[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (- size * counter) + 'px)';
+
+nextBtn.addEventListener('click',() => {
+    if(counter >= carouselImg.length - 1) return;
+    carouselSlide.style.transition = 'transform 0.4s ease-in-out';
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (- size * counter) + 'px)';
+});
+
+carouselSlide.addEventListener('transitionend', () => {
+    if(carouselImg[counter].id === 'lastClone'){
+        carouselSlide.style.transition = 'none';
+        counter = carouselImg.length - 2;
+        carouselSlide.style.transform = 'translateX(' + (- size * counter) + 'px)';
+    }
+    if(carouselImg[counter].id === 'firstClone'){
+        carouselSlide.style.transition = 'none';
+        counter = carouselImg.length - counter;
+        carouselSlide.style.transform = 'translateX(' + (- size * counter) + 'px)';
+    }
+});

908159b16aeb4a0f918c574f9091ecb0396fca88
NikKho77 <68790627+NikKho77@users.noreply.github.com>
2021-01-22 11:54:40 +0200

    Update carusel.js
    wd

--- carusel.js
+++ carusel.js
@@ -8,7 +8,7 @@ const size = carouselImg[0].clientWidth;
 carouselSlide.style.transform = 'translateX(' + (- size * counter) + 'px)';
 
 nextBtn.addEventListener('click',() => {
-    if(counter >= carouselSlide.length - 1) return;
+    if(counter >= carouselImg.length - 1) return;
     carouselSlide.style.transition = 'transform 0.4s ease-in-out';
     counter++;
     carouselSlide.style.transform = 'translateX(' + (- size * counter) + 'px)';

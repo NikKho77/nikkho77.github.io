6147fb6d32076161fa5e6ec9568c290393c985c9
NikKho77 <68790627+NikKho77@users.noreply.github.com>
2021-01-29 14:12:18 +0200

    Update task.2.js
    fw

--- task.2.js
+++ task.2.js
@@ -1,11 +1,21 @@
 const btn = document.querySelector('.b1');
 let div = document.querySelector('.out');
-
+const btn2 = document.querySelector('.b2');
+let div2 = document.querySelector('.out2');
 let count = 0;
 
 btn.onclick = function () {
    if(count < 51){
       div.innerHTML =  count;
       count++;
+   }
+}
+
+btn2.onclick = () => {
+   for(let i = 0;i < 51;i++){
+      if(count < i){
+         div.innerHTML = count + ' ';
+         count++;
+      }
    }
 }

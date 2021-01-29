4a22ee4335da3ab3574e0ac0e35a82806b521922
NikKho77 <68790627+NikKho77@users.noreply.github.com>
2021-01-29 12:43:34 +0200

    Update task.2.js
    cs

--- task.2.js
+++ task.2.js
@@ -3,12 +3,9 @@ let div = document.querySelector('.out');
 
 let count = 0;
 
-function f1(){
-  if(count < 50 ){
-     div.innerHTML = count;
-     count++;
-
-  }
+btn.onclick = function () {
+   if(count < 50){
+      div.innerHTML = count;
+      count++;
+   }
 }
-
-btn.onclick = f1();
